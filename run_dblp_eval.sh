5fa74b62186912befdb9aa6fd4ea501b123e5079
guogy <50547999+ggyhug@users.noreply.github.com>
2022-11-20 19:43:26 +0800

    Delete run_dblp_eval.sh

--- run_dblp_eval.sh
+++
@@ -1,5 +0,0 @@
-./eval simpush dblp 0.05 50 50 > acc_dblp.txt
-./eval simpush dblp 0.02 50 50 >> acc_dblp.txt
-./eval simpush dblp 0.01 50 50 >> acc_dblp.txt
-./eval simpush dblp 0.005 50 50 >> acc_dblp.txt
-./eval simpush dblp 0.002 50 50 >> acc_dblp.txt
